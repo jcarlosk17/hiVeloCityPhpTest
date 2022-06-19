ea8033cc5e3ac1168728de7f912fc4be489d1b46
jcarlosk17 <jslv.junior@gmail.com>
2022-06-19 21:08:35 +0900

    Source code modifications completed.

--- src/FinalResult.php
+++ src/FinalResult.php
@@ -1,40 +1,41 @@
 <?php
 
 class FinalResult {
-    function results($f) {
-        $d = fopen($f, "r");
-        $h = fgetcsv($d);
+    function results($file) {
         $rcs = [];
-        while(!feof($d)) {
-            $r = fgetcsv($d);
-            if(count($r) == 16) {
-                $amt = !$r[8] || $r[8] == "0" ? 0 : (float) $r[8];
-                $ban = !$r[6] ? "Bank account number missing" : (int) $r[6];
-                $bac = !$r[2] ? "Bank branch code missing" : $r[2];
-                $e2e = !$r[10] && !$r[11] ? "End to end id missing" : $r[10] . $r[11];
-                $rcd = [
+        $opened_file = fopen($file, "r");
+        [$currency, $failure_code, $failure_message] = fgetcsv($opened_file);
+
+        while(!feof($opened_file)) {
+            $record = fgetcsv($opened_file);
+
+            if(count($record) == 16) {
+                $amt = !$record[8] || $record[8] == "0" ? 0 : (int) ($record[8] * 100);
+                $ban = !$record[6] ? "Bank account number missing" : (int) $record[6];
+                $bbc = !$record[2] ? "Bank branch code missing" : $record[2];
+                $e2e = !$record[10] && !$record[11] ? "End to end id missing" : $record[10] . $record[11];
+
+				$rcs[] = [
                     "amount" => [
-                        "currency" => $h[0],
-                        "subunits" => (int) ($amt * 100)
+                        "currency" => $currency,
+                        "subunits" => $amt
                     ],
-                    "bank_account_name" => str_replace(" ", "_", strtolower($r[7])),
-                    "bank_account_number" => $ban,
-                    "bank_branch_code" => $bac,
-                    "bank_code" => $r[0],
-                    "end_to_end_id" => $e2e,
+                    "bank_account_name"		=> str_replace(" ", "_", strtolower($record[7])),
+                    "bank_account_number" 	=> $ban,
+                    "bank_branch_code" 		=> $bbc,
+                    "bank_code" 			=> $record[0],
+                    "end_to_end_id" 		=> $e2e,
                 ];
-                $rcs[] = $rcd;
             }
         }
-        $rcs = array_filter($rcs);
-        return [
-            "filename" => basename($f),
-            "document" => $d,
-            "failure_code" => $h[1],
-            "failure_message" => $h[2],
-            "records" => $rcs
+
+		return [
+            "filename" 			=> basename($file),
+            "document" 			=> $opened_file,
+            "failure_code" 		=> $failure_code,
+            "failure_message"	=> $failure_message,
+            "records" 			=> $rcs
         ];
     }
 }
-
 ?>
