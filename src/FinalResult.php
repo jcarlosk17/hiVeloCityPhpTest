a254975436003668d8134bf9da86f1c3415c1193
jcarlosk17 <jslv.junior@gmail.com>
2022-06-22 13:55:44 +0900

    Source code modifications completed.

--- src/FinalResult.php
+++ src/FinalResult.php
@@ -2,6 +2,37 @@
 
 class FinalResult {
     function results($file) {
+		$rcs = [];
+		$contents = array_map("str_getcsv", file($file));
+        [$currency, $failure_code, $failure_message] = array_shift($contents);
+
+		foreach ($contents as $record):
+			if (count($record) > 11) {
+				$rcs[] = [
+					"amount" => [
+						"currency" => $currency,
+						"subunits" => !$record[8] || $record[8] == "0" ? 0 : (int) ($record[8] * 100)
+					],
+					"bank_account_name" 	=> str_replace(" ", "_", strtolower($record[7])),
+					"bank_account_number" 	=> !$record[6] ? "Bank account number missing" : (int) $record[6],
+					"bank_branch_code" 		=> !$record[2] ? "Bank branch code missing" : $record[2],
+					"bank_code" 			=> $record[0],
+					"end_to_end_id" 		=> !$record[10] && !$record[11] ? "End to end id missing" : $record[10] . $record[11]
+				];
+			}
+		endforeach;
+
+		return [
+            "filename" 			=> basename($file),
+            "failure_code" 		=> $failure_code,
+            "failure_message"	=> $failure_message,
+            "records" 			=> $rcs
+        ];
+    }
+}
+
+/* class FinalResult {
+    function results($file) {
         $rcs = [];
         $opened_file = fopen($file, "r");
         [$currency, $failure_code, $failure_message] = fgetcsv($opened_file);
@@ -37,5 +68,5 @@ class FinalResult {
             "records" 			=> $rcs
         ];
     }
-}
+} */
 ?>
